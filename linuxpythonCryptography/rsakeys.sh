36a8b1afa385c03f824f9b8902e2d336d6d96a55
David Wainaina <dwainaina3@gmail.com>
2022-07-06 11:39:45 +0300

    script to create keys working

--- linuxpythonCryptography/rsakeys.sh
+++ linuxpythonCryptography/rsakeys.sh
@@ -5,8 +5,10 @@ CURRENT_DIR=$(pwd)
 
 APP_NAME="linuxpythonCryptography"
 
+##create keys directory
+mkdir keys
 ##create private key with app name as passphrase and 1024 bytes
-openssl genrsa -out $CURRENT_DIR/keys/aptitudeprivatekey.pem -passout pass:${APP_NAME} 1024
+openssl genrsa -out $CURRENT_DIR/keys/privatekey.pem -passout pass:${APP_NAME} 1024
 
 ##pass app name as passphrase and private key to create public key
-openssl rsa -in $CURRENT_DIR/keys/aptitudeprivatekey.pem -passin pass:${APP_NAME} -pubout -out $CURRENT_DIR/configs/configfiles/aptitudepublickey.pem
+openssl rsa -in $CURRENT_DIR/keys/privatekey.pem -passin pass:${APP_NAME} -pubout -out $CURRENT_DIR/keys/publickey.pem
