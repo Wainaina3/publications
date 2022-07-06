6506e37f820359390b384a56e90df9c2c753cd85
David Wainaina <dwainaina3@gmail.com>
2022-07-06 18:28:46 +0300

    Decryption function working

--- linuxpythonCryptography/crypt.py
+++ linuxpythonCryptography/crypt.py
@@ -2,33 +2,51 @@ from base64 import b64decode
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto.PublicKey import RSA
 
-def test_decryption():
-        encrypted_data_file = "encryptedconfig"
+APP_NAME="linuxpythonCryptography"
 
-        private_key_file = "aptitudeprivatekey.pem"
-         
-        with open(private_key_file,'r') as key:
-            #skip first and last line (-----BEGIN RSA PRIVATE KEY----- -----END RSA PRIVATE KEY-----)
-            private_key_lines = key.readlines()[1:-1]
-            private_key_clean = ""
-            #clean the private file into readable format
-            for line in private_key_lines:
-                private_key_clean ="".join([private_key_clean,line.rstrip()])
+##Decrypt encrypted data with RSA private key
+def decrypt_data():
 
-            #working code but with externally cleaned private key
-            #private_key = RSA.importKey(base64.b64decode(key.read()),passphrase="aptitude")
+    clean_private_key = read_encryption_key()
+    encrypted_data = read_encrypted_data()
 
-            #testing with cleaned private key
-            private_key = RSA.importKey(base64.b64decode(private_key_clean),passphrase="aptitude")
+    #import cleaned private  key and passing passphrase used to create key  
+    private_key = RSA.importKey(clean_private_key,passphrase=APP_NAME)
 
-        with open(encrypted_data_file,'r') as ecrypted_file:
-            encrypted_data = ecrypted_file.read()
+    #using PKCS1_OAEP since the private key used the PKCS1_OAEP padding 
+    rsa_cipher = PKCS1_OAEP.new(private_key)
 
-        rsa_cipher = PKCS1_OAEP.new(private_key)
-        dec_data = rsa_cipher.decrypt(base64.b64decode(encrypted_data)).decode()
-        #rsa_cipher = Cipher_PKCS1_v1_5.new(private_key)
-        #dec_data = rsa_cipher.decrypt(base64.b64decode(encrypted_data), None).decode()
-        print("Decrypting encrypted file data")
-        print(dec_data)
+    #decrypt data and decode
+    decrypted_data = rsa_cipher.decrypt(encrypted_data).decode()
 
-test_decryption()
+    print(decrypted_data)
+
+#read private key, clean it and return usable key
+def read_encryption_key():
+    #private key
+    private_key_file = "keys/privatekey.pem"
+
+    with open(private_key_file,'r') as key:
+       #skip first and last line (-----BEGIN RSA PRIVATE KEY----- -----END RSA PRIVATE KEY-----)
+       private_key_lines = key.readlines()[1:-1]
+
+       clean_private_key =""
+       #clean the private key into format readable by RSA importkey
+       for line in private_key_lines:
+            clean_private_key ="".join([clean_private_key,line.rstrip()])
+
+    #b64decode the cleaned private key and return
+    return b64decode(clean_private_key)
+
+#Read encrypted data from file
+def read_encrypted_data():
+    encrypted_data_file = "encrypteddata"
+   
+   #read file and b64 decode for decryption
+    with open(encrypted_data_file,'r') as encrypted_file:
+        encrypted_data = b64decode(encrypted_file.read())
+
+    return encrypted_data
+
+# decrypted encrypteddata file and print
+decrypt_data()
--- linuxpythonCryptography/rsakeys.sh
+++ linuxpythonCryptography/rsakeys.sh
@@ -8,6 +8,8 @@ APP_NAME="linuxpythonCryptography"
 ##create keys directory
 mkdir keys
 ##create private key with app name as passphrase and 1024 is the numbits or keysize in bits
+##numbit selected can only encrypt data which are 245 bytes in size. for large data, use 4096 or 
+#change encryption to AES
 openssl genrsa -out $CURRENT_DIR/keys/privatekey.pem -passout pass:${APP_NAME} 1024
 
 ##pass app name as passphrase and private key to create public key
