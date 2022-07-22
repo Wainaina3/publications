2a686fe6a9d1f50a93cc33fc921ac18caf4b0ff8
David Wainaina <dwainaina3@gmail.com>
2022-07-22 22:43:21 +0300

    added wsgi entry file

--- govt_structure/app.py
+++ govt_structure/app.py
@@ -1,6 +1,6 @@
-import mainapp
+from mainapp import create_app
 
-app = mainapp.create_app()
+app = create_app()
 
 #add host as 0.0.0.0 to be accessible from outside when running as a container
 if __name__ == "__main__":
