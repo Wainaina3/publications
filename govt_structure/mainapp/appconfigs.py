84984732b1829192e668d7819ebcc001c7d6a45e
David Wainaina_server <dwainaina3@gmail.com>
2022-07-24 09:31:37 +0000

    edist on server

---
+++ govt_structure/mainapp/appconfigs.py
@@ -0,0 +1,17 @@
+import yaml
+import os
+
+class Appconfig(object):
+
+    #return config data given a name
+    #if its running in a container, the config_data arg will have to be shared and config name will be secret name
+    def read_configs(config_name,config_data="data"):
+       
+        current_dir = os.getcwd()
+        config_file = "govtconf.yaml"
+        #return local configfiles dir
+        app_secrets_dir = "".join([current_dir,"/mainapp/configs/confs/"])
+        with open(os.path.join(app_secrets_dir, config_file)) as configs_file:
+            loaded_config = yaml.safe_load(configs_file)
+            config = loaded_config[config_name]
+        return config
